27bdaede4c1157d684c9995a779e47cbf4d1da6b
pxlkng <jeremiah.holmes@hotmail.com>
2018-12-31 11:37:43 -0600

    fix php and style form

--- rsvp.php
+++ rsvp.php
@@ -1,30 +1,24 @@
 <?php
-//if (isset($_POST['submit']))  {
-     $myfile = fopen("rsvps.txt", "a") or die("Unable to open file!");
-     //Email information
-     $name = filter_input(INPUT_POST, 'name');
-     $pNumber = filter_input(INPUT_POST, 'pNumber');
-     $device = filter_input(INPUT_POST, 'device');
-     $repair = filter_input(INPUT_POST, 'repair');
+   $myfile = fopen("rsvp.txt", "a") or die("Unable to open file!");
+   // Import form data
+   $name = filter_input(INPUT_POST, 'name');
+   $pNumber = filter_input(INPUT_POST, 'pNumber');
+   $device = filter_input(INPUT_POST, 'device');
+   $repair = filter_input(INPUT_POST, 'repair');
 
 
-     $subject = "New Repair - ";
-     $today = date("M,d,Y h:i:s A");
-     $txt = $today." ".$name." ".$pNumber." ".$device." ".$repair."\n";
-
-
+   $subject = "New Repair - ";
+   $today = date("M,d,Y h:i:s A");
+   $txt = $today." ".$pNumber." ".$device." ".$repair."\n".$name;
 
 
 
-     //mail("jeremiah.holmes@hotmail.com", $subject, $today."\n".$txt);
-
-     fwrite($myfile, $subject.$txt);
-     fclose($myfile);
-     header("Location: http://google.com/");
 
 
+   //mail("jeremiah.holmes@hotmail.com", $subject, $today."\n".$txt);
 
-  // }
-
+   fwrite($myfile, $txt);
+   fclose($myfile);
+   header("Location: http://pxlkng.com/cellphoneUSA");
    die();
  ?>
